d0aaea8fcc57ee7e075160ce56c04b120e6920df
alcasinoroyale <github email address>
2019-06-10 18:30:48 -0400

    Build out the routes for Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+  end
+
+  def show
+    current_user = User.find(params[:id])
+    render json: current_user
+  end
+
+  def favorite_films
+    films = current_user.favorite_films
+    render json: films
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
