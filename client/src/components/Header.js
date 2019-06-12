d5d4241a0dcba19b05cfdcfedc6ae00fff3f4056
alcasinoroyale <github email address>
2019-06-12 18:05:08 -0400

    Add Google Fonts and CSS to visualize layout of app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import banner from '../images/banner.jpg';
 
 const Header = () => {
   return (
     <div className="header">
       <h1>Film Scope</h1>
+      <img src={banner} width="600" alt="Banner" />
     </div>
   )
 }
