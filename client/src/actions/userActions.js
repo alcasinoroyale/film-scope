f1960989fa1b0dbe93d5beee2e1497493c8f72bf
alcasinoroyale <alexzdatny@gmail.com>
2019-06-18 15:39:28 -0400

    Testing json objects in console

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -2,7 +2,10 @@ export function fetchUsers() {
   return (dispatch) => {
     dispatch({type: 'LOAD_USERS' })
     return fetch('/api/users')
-    .then(response => response.json())
-    .then(users => dispatch({ type: 'FETCH_USERS', payload: users }))
+    .then(res => res.text())
+    .then(text => console.log(text))
+    // .then(res => { return res.json()})
+    // .then(users => { return dispatch({ type: 'FETCH_USERS', payload: users })
+    //})
   }
 }
