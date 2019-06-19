5575819ad05b0fcf830b2610bb4ee5f4e332bb79
alcasinoroyale <alexzdatny@gmail.com>
2019-06-19 16:53:56 -0400

    Map users for select menu

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,14 +14,30 @@ class Login extends Component {
   }
 
   render() {
+
+    const mapUsers = this.props.allUsers.map((user, index) => {
+      return (
+        <option key={user.id} value={user.id} id={user.id}>
+          {user.username}
+        </option>
+      )
+    })
+
     return (
         <div className="usersDropdown">
           <h3>Login Page</h3>
           <select onChange={(event) => this.handleChange(event)}>
             <option>Select Your Username</option>
+            {mapUsers}
           </select>
         </div>
     )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    allUsers: state.user.allUsers
   }
 }
 
@@ -29,4 +45,4 @@ const mapDispatchToProps = (dispatch) => bindActionCreators({
   fetchUsers, buildUser },
   dispatch)
 
-export default connect(null, mapDispatchToProps)(Login)
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
