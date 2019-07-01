5e26509d77ed1974bf722e9815ba1c52a1eb451d
alcasinoroyale <alexzdatny@gmail.com>
2019-07-01 15:56:41 -0400

    Modify filmActions and Reducer to be specific to Top Rated

--- client/src/actions/filmActions.js
+++ client/src/actions/filmActions.js
@@ -1,13 +1,10 @@
 const API_KEY = process.env.REACT_APP_FILM_API_KEY;
 
-export function fetchFilms() {
+export function fetchFilms(page) {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_FILMS '});
-    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`)
-    .then(res => {
-        const film = res.data;
-        return res.json() })
-    .then(films => { return dispatch({ type: 'FETCH_FILMS', payload: films.data })
-    })
+    dispatch({ type: 'LOADING_TOP_RATED '});
+    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=${page}`)
+    .then(res => res.json())
+    .then(res => dispatch({ type: 'FETCH_TOP_RATED', response: res }))
   }
 }
--- client/src/reducers/filmsReducer.js
+++ client/src/reducers/filmsReducer.js
@@ -1,14 +1,16 @@
 const initialState = {
   loading: false,
   allFilms: [],
+  currentPage: 1,
+  totalPages: null
 }
 
 export default function filmsReducer(state = initialState, action) {
     switch(action.type) {
-      case 'LOADING_FILMS':
+      case 'LOADING_TOP_RATED':
         return { ...state, loading: true }
-      case 'FETCH_FILMS':
-        return { ...state, loading: false, allFilms: action.payload }
+      case 'FETCH_TOP_RATED':
+        return { ...state, loading: false, allFilms: action.response.results }
       default: return state;
   }
 }
