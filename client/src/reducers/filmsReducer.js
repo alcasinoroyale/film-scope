49d4600e06f26e0be2b0007b65f2792c567a2236
alcasinoroyale <alexzdatny@gmail.com>
2019-07-02 18:36:07 -0400

    Make sure the actions in each case match the initial state

--- client/src/reducers/filmsReducer.js
+++ client/src/reducers/filmsReducer.js
@@ -19,18 +19,30 @@ const initialState = {
 
 export default function filmsReducer(state = initialState, action) {
     switch(action.type) {
+
       case 'LOADING_TOP_RATED':
         return { ...state, loading: true }
       case 'FETCH_TOP_RATED':
-        return { ...state, loading: false, allFilms: action.response.results }
+        return { ...state, loading: false,
+          topRated: { films: action.response.results },
+          page: action.currentPage,
+          totalPages: action.response.total_pages
+        }
+
       case 'LOADING_FILM':
         return { ...state, loading: true}
       case 'DISPLAY_FILM':
         return { ...state, loading: false, film: { filmInfo: action.filmInfo }}
+
       case 'FETCH_NOW_PLAYING':
         return { ...state, loading: true }
       case 'DISPLAY_NOW_PLAYING':
-        return { ...state, loading: false, nowPlaying: action.response.results }
+        return { ...state, loading: false,
+          nowPlaying: { films: action.response.results },
+          page: action.currentPage,
+          totalPages: action.response.total_pages
+        }
+
       default: return state;
   }
 }
