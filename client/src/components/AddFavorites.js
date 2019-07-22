0e79494ffbdb13d0f89bae57e59b4af3000f7e58
alcasinoroyale <alexzdatny@gmail.com>
2019-07-22 18:08:18 -0400

    Only Show Add/Remove Favorites if active user

--- client/src/components/AddFavorites.js
+++ client/src/components/AddFavorites.js
@@ -5,12 +5,18 @@ import { bindActionCreators } from 'redux'
 
 class AddFavorites extends Component {
   render() {
+    if (this.props.activeUser) {
     return (
       <div>
-      <button className="favorite_films" onClick={() => this.props.addToFavorites(this.props.film, this.props.activeUser)}>Add To Favorites</button>
-      <button className="favorite_films" onClick={() => this.props.removeFromFavorites(this.props.film, this.props.activeUser)}>Remove From Favorites</button>
+        <button className="favorite_films" onClick={() => this.props.addToFavorites(this.props.film, this.props.activeUser)}>Add To Favorites</button>
+        <button className="favorite_films" onClick={() => this.props.removeFromFavorites(this.props.film, this.props.activeUser)}>Remove From Favorites</button>
       </div>
-    )
+      )
+    } else {
+      return (
+        <div></div>
+      )
+    }
   }
 }
 
