1c03b1e7a22e6dc0707473b6679976f3a91125df
alcasinoroyale <alexzdatny@gmail.com>
2019-08-19 16:58:53 -0400

    Fix posters that are unavailable

--- client/src/components/FilmCard.js
+++ client/src/components/FilmCard.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const FilmCard = props => {
+  if (props.film.poster_path === null) {
+    return (
+      <div className="filmCard">
+        <Link key={props.film.id} to={`/films/${props.film.id}`} >
+          <img alt={props.film.title} src="https://critics.io/img/movies/poster-placeholder.png" />
+          <div className="unavailable">
+            <h3>{props.film.title}</h3>
+          </div>
+        </Link>
+      </div>
+    )
+  }
   return (
     <div className="filmCard">
       <Link key={props.film.id} to={`/films/${props.film.id}`} >
-        <img alt="film poster" src={`https://image.tmdb.org/t/p/w154${props.film.poster_path}`} />
+        <img alt={props.film.title} src={`https://image.tmdb.org/t/p/w154${props.film.poster_path}`} />
       </Link>
     </div>
   )
