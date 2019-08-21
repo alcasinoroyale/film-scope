30b21f6cbde99cce3300b6e5eb99eac0b9309c03
alcasinoroyale <alexzdatny@gmail.com>
2019-08-21 15:05:48 -0400

    Align lines of code

--- client/src/components/FilmInfo.js
+++ client/src/components/FilmInfo.js
@@ -13,25 +13,25 @@ class FilmInfo extends Component {
     console.log(this.props.film)
     return (
       <div className="filmInfo">
-          <h1>{this.props.film.title}</h1>
-          <h3>{this.props.film.tagline}</h3>
+        <h1>{this.props.film.title}</h1>
+        <h3>{this.props.film.tagline}</h3>
           <img src={`https://image.tmdb.org/t/p/w342${this.props.film.poster_path}`} alt="poster" />
-          <div className="moreInfo">
-          <AddFavorites />
-          <h3>Synopsis</h3>
-          <p>{this.props.film.overview}</p>
-          <h3>Cast</h3>
-          <p>{this.props.credits.cast.slice(0, 18).map((cast, index) => {
-            return `${cast.name} as ${cast.character}`
-          }).join(" * ")}</p>
-          <h3>Details | <a className="see-more" href={`https://www.themoviedb.org/movie/${this.props.film.id}`} target="_blank" rel="noopener noreferrer">See More Info</a></h3>
-          <p>{this.props.credits.crew.slice(0, 4).map((crew, index) => {
-            return `${crew.job}: ${crew.name}`}).join(", ")}</p>
-          <p>Release Date: {this.props.film.release_date} | Runtime: {this.props.film.runtime} Minutes</p>
-          <p>Genres: {this.props.genres.map((genre, index) => {
-            return genre.name
-          }).join(", ")}</p>
-        </div>
+            <div className="moreInfo">
+              <AddFavorites />
+              <h3>Synopsis</h3>
+              <p>{this.props.film.overview}</p>
+                <h3>Cast</h3>
+                  <p>{this.props.credits.cast.slice(0, 18).map((cast, index) => {
+                    return `${cast.name} as ${cast.character}`
+                  }).join(" * ")}</p>
+                <h3>Details | <a className="see-more" href={`https://www.themoviedb.org/movie/${this.props.film.id}`} target="_blank" rel="noopener noreferrer">See More Info</a></h3>
+                  <p>{this.props.credits.crew.slice(0, 4).map((crew, index) => {
+                    return `${crew.job}: ${crew.name}`}).join(", ")}</p>
+                  <p>Release Date: {this.props.film.release_date} | Runtime: {this.props.film.runtime} Minutes</p>
+                  <p>Genres: {this.props.genres.map((genre, index) => {
+                    return genre.name
+                  }).join(", ")}</p>
+            </div>
       </div>
     )
   }
